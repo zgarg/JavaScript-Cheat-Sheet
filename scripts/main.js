94d8884cd5b31983879e309b5bfacc4b028a1b95
Zoya Garg <zoyagarg111@gmail.com>
2016-07-14 15:14:04 -0400

    FINAL CODE

---
+++ scripts/main.js
@@ -0,0 +1,76 @@
+alert("This is Zoya's JavaScript Cheat Sheet");
+alert("Hello World") //these are alerts
+
+console.log("Hello World"); //this is debugging using the console
+
+var x = 3
+var y = 4
+
+console.log(x + y); //this is a variable
+
+var juices = ["Orange, Apple"]
+var sodas = ["Coke, Pepsi"]
+var drinks = ["Juices, Sodas"]
+console.log(juices, sodas, drinks); //this is a multi-dimensional array
+
+var user = {firstName: 'Zoya', lastName: 'Garg'};
+console.log(user.firstName); //this is dot notation 
+
+var desserts = {make: 'cookie', model: 'cake'};
+console.log(desserts['make']);// this is an object data table
+
+console.log(10<5);// this is an example of testing
+
+if(5>10){
+ console.log("You'll never see this in the console because 5 is not greater than 10");
+}
+if(5<10){
+ console.log("But you'll definitely see this");
+}// this is a if statement
+
+if(5>10){
+ console.log("You'll never see this because 5 is not greater than 10");
+} else{
+ console.log("You will see this though, since 5 < 10 evaluates to false");
+} //this is an else statement
+
+//If and else statements are both examples of logic
+
+function someName(numberOne, otherNumber){
+ return numberOne + 10 + otherNumber;
+}
+console.log(someName(4, 14)); //this is a function
+
+var sports = ["Squash, Swimming"]
+var school = ["Math, Science"]
+var hobbies = ["Sports, School"]
+console.log(sports, school, hobbies); //this is a multi-dimensional array that completes number 4
+
+var a=7
+var b=9
+console.log(a+b)
+
+var string1=12
+var string2=13
+console.log(string1+string2) //this is a script with numbers and strings that completes number 3
+
+var x=3
+if(x<10){
+	alert("This variable is less than 10")} //this was a scrip that checks if a variable is less than 10 that completes number 5
+
+function someThing(numberOne, otherNumber){
+ return numberOne + 10 + otherNumber;
+}
+alert(someThing(4, 14));
+
+function someNumbers(numberThree, otherNumber){
+ return numberThree + 10 + otherNumber;
+}
+alert(someName(5, 6));
+
+function someFunction(numberFive, otherNumber){
+ return numberFive + 10 + otherNumber;
+}
+alert(someFunction(2, 3));
+
+
